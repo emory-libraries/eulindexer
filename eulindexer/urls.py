f8f3ffc3e722dab1d4d2e5566f6c279ea110b343
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2011-06-29 11:07:20 -0400

    add a redirect from / to admin/ since all we have is an admin site for now

--- eulindexer/urls.py
+++ eulindexer/urls.py
@@ -1,9 +1,11 @@
 # urls.py
 from django.conf.urls.defaults import *
 from django.contrib import admin
+from django.views.generic.simple import redirect_to
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    (r'^$', redirect_to, {'url': '/admin/'}),
     (r'^admin/', include(admin.site.urls)),
 )
