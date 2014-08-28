df18345d520cdb80e0c4b64480455913746bf851
rlskoeser <rebecca.s.koeser@emory.edu>
2014-08-28 17:52:57 -0400

    updates for django 1.6.x

--- eulindexer/urls.py
+++ eulindexer/urls.py
@@ -1,11 +1,11 @@
 # urls.py
-from django.conf.urls.defaults import *
+from django.conf.urls import patterns, include
 from django.contrib import admin
-from django.views.generic.simple import redirect_to
+from django.views.generic.base import RedirectView
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^$', redirect_to, {'url': 'admin/'}),
+    (r'^$', RedirectView.as_view(url='admin/')),
     (r'^admin/', include(admin.site.urls)),
 )
